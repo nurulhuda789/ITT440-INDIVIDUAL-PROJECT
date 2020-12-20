e6e88681b4186d94233d66fe3bd03994fc9286b8
nurulhuda789 <hudasallehudin789@gmail.com>
2020-12-20 13:30:10 +0800

    POP3 CLIENT

--- pop3client.py
+++ pop3client.py
@@ -3,13 +3,13 @@ import sys
 
 host = '192.168.43.81'
 port = 8995
-pop3client = socket.socket(socket.AP_INET, socket.SOCK_STREAM)
+pop3client = socket.socket()
 
 pop3client.connect((host,port))
 print(pop3client.recv(1024).decode())
 
 while True:
-	print("Masukkan  command(USER|PASS|STAT|LIST|RETR|DELE|QUIT) : ")
+	print("Masukkan  command(USER | PASS | STAT | LIST | RETR | DELE | QUIT) : ")
 	command = input('')
 	pop3client.send(command.encode())
 	
